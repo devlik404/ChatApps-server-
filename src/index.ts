1fd8207971f2c90b3f85071cd9e03dae605caf97
devlik404 <devfajarmalik@gmail.com>
2023-12-26 22:58:43 +0700

    feat:socket get and create

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,104 @@
-import cors from 'cors';
-import express from 'express';
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-import { ClientToServerEvents, ServerToClientEvents } from '../../typings';
-import router from './router';
-import { PrismaClient } from '@prisma/client';
+import cors from "cors";
+import express, { query } from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import { ClientToServerEvents, ServerToClientEvents } from "../../typings";
+import router from "./router";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/', router);
+app.use("/", router);
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
-
-const connectedUser: Record<string, string> = {};
 
 io.use((socket, next) => {
   if (socket.handshake.auth && socket.handshake.auth.token) {
     next();
   } else {
-    next(new Error('Not Authorized'));
+    next(new Error("Not Authorized"));
   }
 });
 
-io.on('connection', (socket: Socket<ClientToServerEvents, ServerToClientEvents>) => {
-  // ID pengguna yang terhubung
+const connectedUsers: Record<string, string> = {};
+
+io.on("connection", (socket: Socket) => {
   const senderId = socket.handshake.query.id as string | undefined;
+  console.log("senderId:", senderId);
 
+  // save user connected if login uccses
   if (senderId !== undefined) {
-    // simpan ke connectedUser
-    connectedUser[senderId] = socket.id;
-    console.log('Pengguna terhubung dengan ID:', senderId);
-  } else {
-    console.error('ID Pengirim tidak terdefinisi.');
+    connectedUsers[senderId] = socket.id;
   }
 
-  socket.on('clientMsg', async (data) => {
+  io.sockets.emit("user_connected", { clientId: socket.id });
+
+  socket.on("clientMsg", async (data) => {
+    console.log("clientMsg:", data);
     try {
-      const userIdSender = socket.handshake.query.id as string 
-      console.log("userIdSender",userIdSender);
-      
-      const userIdReceiver = data.receiverId; 
-      console.log("userIdReceiver",userIdReceiver);
-      
+      if(data.receiverId){
+       await prisma.message.create({
+        data: {
+          content: data.content,
+          senderId: data.senderId,
+          receiverId: data.receiverId,
+        },
+      });
 
-      const createMsg = await prisma.message.create({
-        data:{
-          content:data.content,
-          senderId:userIdSender,
-          receiverId:userIdReceiver,
-          timestamp: new Date(),
-        }
-      })
-      
+      const recipientSocket = connectedUsers[data.receiverId];
+      console.log("recipientSocketr", recipientSocket);
 
-      const recipientSocket = connectedUser[userIdReceiver];
-      console.log("recipientSocketr",recipientSocket);
-      
-      if (recipientSocket) {
-        io.to(recipientSocket).emit('serverMsg', {
-          senderId: createMsg.senderId,
-          receiverId: createMsg.receiverId,
-          content: createMsg.content,
-          timestamp: createMsg.timestamp.toISOString(),
+      // get message from mongo
+      const getLatestMessage = async (senderId: string, receiverId: string) => {
+        return await prisma.message.findFirst({
+          where: {
+            AND: [{ senderId: senderId }, { receiverId: receiverId }],
+          },
+          orderBy: {
+            timestamp: "desc",
+          },
         });
+      };
 
-      }
-      else{
-        console.log("errrorororo");
-        
-      }
-      console.log('Pesan berhasil dikirim:', createMsg);
+      // get new message
+      const latestMessage = await getLatestMessage(
+        data.senderId,
+        data.receiverId,
+      );
+      console.log("latestMessage",latestMessage);
+      
+        // send new message to client
+        if (latestMessage) {
+          socket.join(recipientSocket);
+          io.to(recipientSocket).emit("serverMsg", {
+            senderId: latestMessage.senderId,
+            receiverId: latestMessage.receiverId,
+            content: latestMessage.content,
+            timestamp: latestMessage.timestamp.toISOString(),
+          });
+       
+          console.log('Pesan terkini berhasil dikirim ke klien:', latestMessage);
+        }
 
+        } else {
+        io.sockets.emit("serverMsg", data);
+      }
     } catch (error) {
-      console.error('Gagal membuat pesan:', error);
+      console.log(error);
     }
- 
   });
-
- 
 });
+
 server.listen(3000, () => {
-  console.log('Server berjalan di http://localhost:3000');
-});
-
-//clean chace prisma
-process.on('beforeExit', async () => {
-  await prisma.$disconnect();
+  console.log("Server berjalan di http://localhost:3000");
 });
